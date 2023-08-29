fbb708158ae18673f9c1a69b02d0898dd237978e
elalitte <eric@lalitte.com>
2023-08-29 14:35:35 +0200

    added new nvim v2

--- nvim/lua/elalitte/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
--- Configuring bufferline
-require("bufferline").setup{}
-local bufferline = require('bufferline')
-bufferline.setup({
-    options = {
-        style_preset = bufferline.style_preset.slant
-    }
-})
-vim.o.background = "dark" -- or "light" for light mode
