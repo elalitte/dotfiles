231d36bbf2b012e80a277d2d07b29b0b37bc11f7
elalitte <eric@lalitte.com>
2026-01-19 11:04:16 +0100

    Update Node.js major version from 20 to 25

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -89,7 +89,7 @@ else
   sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg sudo
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
   # Version à changer en fonction de l'évolution de node (à voir si scriptable)
-  NODE_MAJOR=20
+  NODE_MAJOR=25
   echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
   sudo apt-get update && sudo apt-get install nodejs -y
 fi
